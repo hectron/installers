586c45eadbd1bb6cc665911a6064d38e5f5a16c4
Hector Rios <that.hector@gmail.com>
2022-05-21 22:45:34 -0500

    Add custom env

---
+++ linux/000_setup_env.sh
@@ -0,0 +1,13 @@
+#/usr/bin/env bash
+
+echo "Setting up me directory"
+
+mkdir $HOME/me
+
+echo "Setting up code directory"
+
+mkdir $HOME/code
+
+echo "Setting up $HOME/.local/bin"
+
+mkdir -p $HOME/.local/bin
